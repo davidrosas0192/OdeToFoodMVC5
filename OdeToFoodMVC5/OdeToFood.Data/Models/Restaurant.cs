234a8a6c05e1d5877bd5985d9c6eab198cdb43d5
DAVID <32917160+davidrosas0192@users.noreply.github.com>
2020-05-05 09:24:18 -0600

    adding model Restaurant + enum cuisine type + service IRestaurant Data

---
+++ OdeToFoodMVC5/OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public CuisineType Cuisine { get; set; }
+
+
+    }
+}
---
+++ OdeToFoodMVC5/OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+
+    }
+}
