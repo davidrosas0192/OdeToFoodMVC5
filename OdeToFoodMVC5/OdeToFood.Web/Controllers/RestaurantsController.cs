c827e5435db6865091edea1f48eeed7f5fcb7d99
DAVID <32917160+davidrosas0192@users.noreply.github.com>
2020-05-06 23:13:31 -0600

    Validation with data annotation

--- OdeToFoodMVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFoodMVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -44,10 +44,7 @@ namespace OdeToFood.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Restaurant restaurant)
         {
-            if(string.IsNullOrWhiteSpace(restaurant.Name))
-            {
-                ModelState.AddModelError(nameof(restaurant.Name), "The name is required");
-            }
+         
             if(ModelState.IsValid)
             {
                 db.Add(restaurant);
