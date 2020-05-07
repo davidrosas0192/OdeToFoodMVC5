8e17a1bb0424dbfd571b380ccb876d3f0b574f8d
DAVID <32917160+davidrosas0192@users.noreply.github.com>
2020-05-06 23:07:55 -0600

    adding validation V.1

--- OdeToFoodMVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFoodMVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -44,7 +44,16 @@ namespace OdeToFood.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Restaurant restaurant)
         {
-            db.Add(restaurant);
+            if(string.IsNullOrWhiteSpace(restaurant.Name))
+            {
+                ModelState.AddModelError(nameof(restaurant.Name), "The name is required");
+            }
+            if(ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                return View();
+            }
+            
             return View();
         }
     }
